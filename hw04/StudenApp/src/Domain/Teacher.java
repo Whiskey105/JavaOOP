e04480e462481c713eb0182ed1cc829d347c41c1
Whiskey105 <99003120+Whiskey105@users.noreply.github.com>
2024-03-02 15:28:29 +1000

    Delete hw04/StudenApp directory

--- hw04/StudenApp/src/Domain/Teacher.java
+++
@@ -1,9 +0,0 @@
-package Domain;
-
-public class Teacher extends WorkingPerson{
-    private String acadDegree;
-    public Teacher(String firstName, int age, String acadDegree) {
-        super(firstName, age);
-        this.acadDegree = acadDegree;
-    }
-}
--- hw04/StudenApp/src/Services/iPersonService.java
+++
@@ -1,8 +0,0 @@
-package Services;
-
-import java.util.List;
-
-public interface iPersonService<T> {
-    List<T> getAll();
-    void create(String name, int age);
-}
